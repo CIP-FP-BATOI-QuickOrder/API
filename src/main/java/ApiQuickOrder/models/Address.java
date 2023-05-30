642b85c9ecaf0b1ef92170be41c0da559111dba3
Pablo Sanjuan Muñoz <pabsanmun@alu.edu.gva.es>
2023-05-30 16:28:49 +0200

    Updat address

--- src/main/java/ApiQuickOrder/models/Address.java
+++ src/main/java/ApiQuickOrder/models/Address.java
@@ -14,105 +14,116 @@ import java.util.Set;
 @Table(name = "address", catalog = "QuickOrder")
 public class Address implements java.io.Serializable {
 
-	private static final long serialVersionUID = 1L;
-	private Integer id;
-	@JsonIgnore
-	private User user;
-	private String address;
-	private Integer number;
-	private String name;
-	private String city;
-	private Integer cp;
-	@JsonIgnore
-	private Set<Order> orders = new HashSet<Order>(0);
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    @JsonIgnore
+    private User user;
+    private String address;
+    private Integer number;
+    private String name;
+    private String city;
+    private String addressName;
+    private Integer cp;
+    @JsonIgnore
+    private Set<Order> orders = new HashSet<Order>(0);
 
-	public Address() {
-	}
+    public Address() {
+    }
 
-	public Address(User user, String address, Integer number, String name, String city, Integer cp, Set<Order> orders) {
-		this.user = user;
-		this.address = address;
-		this.number = number;
-		this.name = name;
-		this.city = city;
-		this.cp = cp;
-		this.orders = orders;
-	}
+    public Address(User user, String address, Integer number, String name, String city, Integer cp, Set<Order> orders, String addressName) {
+        this.user = user;
+        this.address = address;
+        this.number = number;
+        this.name = name;
+        this.city = city;
+        this.cp = cp;
+        this.orders = orders;
+        this.addressName = addressName;
+    }
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-	@Column(name = "id", unique = true, nullable = false)
-	public Integer getId() {
-		return this.id;
-	}
+    @Column(name = "id", unique = true, nullable = false)
+    public Integer getId() {
+        return this.id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_id")
-	public User getUser() {
-		return this.user;
-	}
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    public User getUser() {
+        return this.user;
+    }
 
-	public void setUser(User user) {
-		this.user = user;
-	}
+    public void setUser(User user) {
+        this.user = user;
+    }
 
-	@Column(name = "address", length = 100)
-	public String getAddress() {
-		return this.address;
-	}
+    @Column(name = "address", length = 100)
+    public String getAddress() {
+        return this.address;
+    }
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
-	@Column(name = "number")
-	public Integer getNumber() {
-		return this.number;
-	}
+    @Column(name = "number")
+    public Integer getNumber() {
+        return this.number;
+    }
 
-	public void setNumber(Integer number) {
-		this.number = number;
-	}
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
 
-	@Column(name = "name", length = 100)
-	public String getName() {
-		return this.name;
-	}
+    @Column(name = "address_name", length = 30)
+    public String getAddressName() {
+        return this.addressName;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setAddressName(String addressName) {
+        this.addressName = addressName;
+    }
 
-	@Column(name = "city", length = 100)
-	public String getCity() {
-		return this.city;
-	}
+    @Column(name = "name", length = 100)
+    public String getName() {
+        return this.name;
+    }
 
-	public void setCity(String city) {
-		this.city = city;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	@Column(name = "cp")
-	public Integer getCp() {
-		return this.cp;
-	}
+    @Column(name = "city", length = 100)
+    public String getCity() {
+        return this.city;
+    }
 
-	public void setCp(Integer cp) {
-		this.cp = cp;
-	}
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "address")
-	public Set<Order> getOrders() {
-		return this.orders;
-	}
+    @Column(name = "cp")
+    public Integer getCp() {
+        return this.cp;
+    }
 
-	public void setOrders(Set<Order> orders) {
-		this.orders = orders;
-	}
+    public void setCp(Integer cp) {
+        this.cp = cp;
+    }
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "address")
+    public Set<Order> getOrders() {
+        return this.orders;
+    }
+
+    public void setOrders(Set<Order> orders) {
+        this.orders = orders;
+    }
 
 }
